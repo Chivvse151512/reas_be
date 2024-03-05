10fda14ea1c087b6c914bace9744de723c8c9cf0
Chivvse151512 <97072169+Chivvse151512@users.noreply.github.com>
2024-03-05 21:30:12 +0700

    setup

---
+++ reas/Controllers/BidController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace reas.Controllers
+{
+    public class BidController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
