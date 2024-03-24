2872e6a56789c173979dd834fda6090f27be956c
truonghoang <boybybame@gmail.com>
2024-03-25 03:27:36 +0700

    complete Transaction

---
+++ repository/interface/ITransactionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BusinessObject;
+
+namespace repository
+{
+	public interface ITransactionRepository
+	{
+        Task CreateTransactionAsync(Transaction transaction);
+        IQueryable<Transaction> GetAllTransaction();
+        IQueryable<Transaction> GetTransactionsByUserId(int id);
+        Transaction GetTransactionById(int id);
+        Task UpdateTransactionAsync(Transaction transaction);
+        Task DeleteTransactionAsync(int id);
+    }
+}
+
